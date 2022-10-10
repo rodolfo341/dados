610eba380c9d6eb76aea6bb1338694752b7c7191
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-10 18:57:39 -0300

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+    dadoIzquierdo: String = '../assets/img/dice1.png';
+    dadoDerecho: String = '../assets/img/dice4.png';
+
+    num1: number = 1;
+    num2: number = 4;
+
+    tirarDados(): void {
+        this.num1 = Math.round( Math.random() * 5 ) + 1 ;
+        this.dadoIzquierdo = "../assets/img/dice" + this.num1 +".png";
+        this.num2 = Math.round( Math.random() * 5 ) +1 ;
+        this.dadoDerecho = "../assets/img/dice" + this.num2 +".png";
+    }
 }
